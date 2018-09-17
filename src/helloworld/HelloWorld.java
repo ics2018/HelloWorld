33dc7ab635c48e67a05865b16cd3247a4d432ff2
rkaune <rkaune@7HPL6Y1.Student.UGDSB.ED>
2018-09-17 09:58:35 -0400

    This is the HelloWorld code from Unit 2 Activity 1

--- src/helloworld/HelloWorld.java
+++ src/helloworld/HelloWorld.java
@@ -11,12 +11,13 @@ package helloworld;
  * @author rkaune
  */
 public class HelloWorld {
-
+    
     /**
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        System.out.println("I like to code!");
+        System.out.println("Hello World");
+        //System.out.println("I like to code!");
     }
-    
+                       
 }
